fb43338542fc6868bc2add09946a4a48000fa659
malek00 <malek.derbel1@gmail.com>
2021-02-26 09:56:19 +0100

    update pipeline

--- main.tf
+++ main.tf
@@ -4,6 +4,17 @@ provider "azurerm" {
     }
 }
 
+terraform {
+  backend "azurerm" {
+    resource_group_name = "tf_rg_blobstore"
+    storage_account_name = "mdtfstorageaccount"
+    container_name = "tfstate"
+    key = "terraform.tfstate"
+  }
+  
+}
+
+
 resource "azurerm_resource_group" "tf_test"{
 name = "tfmainrg"
 location="francecentral"
